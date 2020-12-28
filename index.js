14bbb1a8765dcf81e37ab320b13bec99b4737744
Aaron Machin <machinaaron1@gmail.com>
2020-12-28 17:14:58 +0000

    add: index

---
+++ index.js
@@ -0,0 +1,19 @@
+const root = process.cwd();
+const config = require( root + '/config' );
+
+const { Discover } = require( root + '/src/discover.js' );
+const { Parse } = require( root + '/src/parse/parse.js' );
+
+const main = () => {
+
+	const test_path = root + '/test/';
+
+	const discover = new Discover( test_path );
+	const parse = new Parse();
+
+	parse.css( discover.css() );
+	parse.html( discover.html() );
+	//parse.js( discover.js() );
+};
+
+main();
